6a70ecfb025b4fa8692959963f3d5bd09e5665ed
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2015-01-13 10:38:54 +0000

    Initial commit

---
+++ Solutions/Endjin.SpecFlow/Extensions/PathExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace System
+{
+    #region Using Directives
+
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    #endregion
+
+    public static class PathExtensions
+    {
+        public static string ResolveBaseDirectory(this string path)
+        {
+            return Path.Combine(GetBaseDirectory(), path);
+        }
+
+        public static string GetBaseDirectory()
+        {
+            var uri = new UriBuilder(Assembly.GetExecutingAssembly().CodeBase);
+
+            return Path.GetDirectoryName(Uri.UnescapeDataString(uri.Path));
+        }
+    }
+}
---
+++ Solutions/Endjin.SpecFlow/Hooks/SetupHooks.cs
@@ -0,0 +1,35 @@
+﻿namespace Endjin.SpecFlow.Hooks
+{
+    #region Using Directives
+
+    using Endjin.Core.Composition;
+    using Endjin.Core.Container;
+
+    using TechTalk.SpecFlow;
+
+    #endregion
+
+    [Binding]
+    public class SetupHooks
+    {
+        [BeforeFeature("container_feature_setup")]
+        public static void ContainerFeatureSetup()
+        {
+            InitializeContainer();
+        }
+
+        [BeforeScenario("container_scenario_setup")]
+        public static void ContainerScenarioSetup()
+        {
+            InitializeContainer();
+        }
+
+        private static void InitializeContainer()
+        {
+            if (ApplicationServiceLocator.Container == null)
+            {
+                ApplicationServiceLocator.InitializeAsync(new Container(), new DesktopBootstrapper()).Wait();
+            }
+        }
+    }
+}
