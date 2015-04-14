bdfeab6624d5840a5318dde7e1261f2094ef62aa
Mike Larah <mike.larah@endjin.com>
2015-04-14 11:43:06 +0100

    Added Endjin.SpecFlow.Azure.Storage with hook to start emulator.

---
+++ Solutions/Endjin.SpecFlow.Azure.Storage/Helpers/AzureEmulatorHelper.cs
@@ -0,0 +1,53 @@
+﻿namespace Endjin.SpecFlow.Azure.Storage.Helpers
+{
+    #region Using Directives
+
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    #endregion
+
+    internal static class AzureEmulatorHelper
+    {
+        private const string WaStorageEmulatorFilePath = @"Microsoft SDKs\Azure\Storage Emulator\WAStorageEmulator.exe";
+        private const string OldWaStorageEmulatorFilePath = @"Microsoft SDKs\Windows Azure\Storage Emulator\WAStorageEmulator.exe";
+
+        public static void Start()
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFilesX86), WaStorageEmulatorFilePath);
+            var info = new FileInfo(path);
+
+            if (!info.Exists)
+            {
+                path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFilesX86), OldWaStorageEmulatorFilePath);
+                info = new FileInfo(path);
+            }
+
+            if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(info.FullName)).Length > 0)
+            {
+                return;
+            }
+
+            Execute("start", path);
+        }
+
+        private static void Execute(string argument, string path)
+        {
+            var start = new ProcessStartInfo { Arguments = argument, FileName = path };
+
+            using (var proc = new Process { StartInfo = start })
+            {
+                proc.Start();
+                proc.WaitForExit();
+
+                var exitCode = proc.ExitCode;
+
+                if (exitCode == 0)
+                {
+                    throw new InvalidOperationException(string.Format("Error {0} executing {1} {2}", exitCode, start.FileName, start.Arguments));
+                }
+            }
+        }
+    }
+}
---
+++ Solutions/Endjin.SpecFlow.Azure.Storage/Hooks/StorageEmulatorHooks.cs
@@ -0,0 +1,20 @@
+﻿namespace Endjin.SpecFlow.Azure.Storage.Hooks
+{
+    #region Using directives
+
+    using Endjin.SpecFlow.Azure.Storage.Helpers;
+
+    using TechTalk.SpecFlow;
+
+    #endregion
+
+    [Binding]
+    public class StorageEmulatorHooks
+    {
+        [BeforeScenario("storage_emulator")]
+        public static void StorageEmulatorSetup()
+        {
+            AzureEmulatorHelper.Start();
+        }
+    }
+}
